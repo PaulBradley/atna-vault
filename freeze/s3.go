4fa996e4d8880f45e91212abe3289b6cb744a277
Paul Bradley <contact@paulbradley.org>
2021-12-10 13:22:17 +0000

    change s3 upload function to support the raw XML being uploaded if gzipping fails. closes #2

--- freeze/s3.go
+++ freeze/s3.go
@@ -14,6 +14,9 @@ import (
 )
 
 func (v *vault) s3UploadFile() {
+	var s3Body *bytes.Reader
+	var s3ContentLength int64
+	var s3ContentType string
 	var s3session *session.Session
 
 	s3session, v.err = session.NewSession(&aws.Config{
@@ -29,21 +32,30 @@ func (v *vault) s3UploadFile() {
 	}
 
 	if v.gzValid {
-		_, v.err = s3.New(s3session).PutObject(&s3.PutObjectInput{
-			Bucket:               aws.String(*v.aws_s3_bucket),
-			Key:                  aws.String(v.outputFilenamePrefix + v.outputFilename),
-			ACL:                  aws.String("private"),
-			Body:                 bytes.NewReader([]byte(v.gzBuffer.Bytes())),
-			ContentLength:        aws.Int64(int64(binary.Size(v.gzBuffer.Bytes()))),
-			ContentType:          aws.String(http.DetectContentType([]byte(v.gzBuffer.Bytes()))),
-			ServerSideEncryption: aws.String("AES256"),
-			StorageClass:         aws.String("GLACIER_IR"),
-		})
-
-		if v.err != nil {
-			log.Println("WARN:" + v.err.Error())
-		} else {
-			log.Println("INFO:Uploaded " + v.outputFilenamePrefix + v.outputFilename)
-		}
+		s3Body = bytes.NewReader([]byte(v.gzBuffer.Bytes()))
+		s3ContentLength = int64(binary.Size(v.gzBuffer.Bytes()))
+		s3ContentType = http.DetectContentType([]byte(v.gzBuffer.Bytes()))
+	} else {
+		s3Body = bytes.NewReader([]byte(v.auditMessage))
+		s3ContentLength = int64(len(v.auditMessage))
+		s3ContentType = "text/xml"
 	}
+
+	_, v.err = s3.New(s3session).PutObject(&s3.PutObjectInput{
+		Bucket:               aws.String(*v.aws_s3_bucket),
+		Key:                  aws.String(v.outputFilenamePrefix + v.outputFilename),
+		ACL:                  aws.String("private"),
+		Body:                 s3Body,
+		ContentLength:        aws.Int64(s3ContentLength),
+		ContentType:          aws.String(s3ContentType),
+		ServerSideEncryption: aws.String("AES256"),
+		StorageClass:         aws.String("GLACIER_IR"),
+	})
+
+	if v.err != nil {
+		log.Println("WARN:" + v.err.Error())
+	} else {
+		log.Println("INFO:Uploaded " + v.outputFilenamePrefix + v.outputFilename)
+	}
+
 }
